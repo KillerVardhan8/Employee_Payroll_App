cdd07a3e0c1f0e4d4289c907f1f9ef3b9767657f
Surya Vardhan <suryavardhankarella@gmail.com>
2025-05-17 14:17:13 +0530

    Employee_Payroll_App

---
+++ src/main/java/net/suryavardhan/employeepayrollapp/EmployeePayrollAppUc1Application.java
@@ -0,0 +1,13 @@
+package net.suryavardhan.employeepayrollapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeePayrollAppUc1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeePayrollAppUc1Application.class, args);
+	}
+
+}
---
+++ src/test/java/net/suryavardhan/employeepayrollapp/EmployeePayrollAppUc1ApplicationTests.java
@@ -0,0 +1,13 @@
+package net.suryavardhan.employeepayrollapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeePayrollAppUc1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
